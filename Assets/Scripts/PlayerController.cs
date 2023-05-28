0a97dcc5f5dbe7b6e20b4c230f1854ba8d0a05ee
Kenichi Takahashi <takahashi.kenichi@t.iput.ac.jp>
2023-05-29 00:22:44 +0900

    Playerの移動機能を追加

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float verticalInput = Input.GetAxis("Vertical");
+        rb.AddForce(Vector3.forward * verticalInput);
     }
 }
