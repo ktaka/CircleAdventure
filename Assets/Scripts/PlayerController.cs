c9b810d9d304afb4edc827aa02d93e3a025bfb75
Kenichi Takahashi <takahashi.kenichi@t.iput.ac.jp>
2023-05-29 00:16:11 +0900

    Revert "Player操作を追加"
    This reverts commit e7f3037c7a5952fd906b10742375329e1fa8aa93.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,17 +4,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        float verticalInput = Input.GetAxis("Vertical");
-        rb.AddForce(Vector3.forward * verticalInput);
+        
     }
 }
